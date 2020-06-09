2fff2afd68f89719137115c5f1e14d8cee359179
Sainell <>
2020-06-09 22:39:24 +0300

    create prototype

---
+++ Assets/Scripts/ObjectClick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectClick : MonoBehaviour, IPointerClickHandler
+{
+    public static event Action<string> clickEvent;
+
+    public string CallObjectTag ()
+    {
+        var tag = gameObject.tag;
+        Debug.Log($"My name is {tag}");
+        return tag;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {      
+        clickEvent?.Invoke(CallObjectTag());
+    }
+}
---
+++ Assets/Scripts/ObjectGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGenerator : MonoBehaviour
+{
+    public List<GameObject> objectList = new List<GameObject>();
+    public int num;
+    public GameObject tempObject;
+
+    private void Awake()
+    {
+        ObjectClick.clickEvent += CheckObjects;
+    }
+    void Start()
+    {
+        GenerateObject();
+    }
+
+    public void GenerateObject()
+    {
+        num = Random.Range(0, 3);
+      tempObject =  Instantiate(objectList[num], transform.position, transform.rotation, transform);
+    }
+
+    public void CheckObjects(string tag)
+    {
+        if (tag.Equals(objectList[num].tag))
+        {
+            Destroy(tempObject);
+            GenerateObject();
+        }
+        else
+        {
+            Debug.Log("Choose other object");
+        }
+    }
+}
