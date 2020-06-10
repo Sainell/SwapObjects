260c3f351a525a4ab1afa7e6b2d3a8fd40388cdc
Sainell <>
2020-06-10 17:06:31 +0300

    add img
    add working prototype
    random generate and choosing

--- Assets/Scripts/ObjectGenerator.cs
+++ Assets/Scripts/ObjectGenerator.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectGenerator : MonoBehaviour
 {
     public List<GameObject> objectList = new List<GameObject>();
-    public int num;
-    public GameObject tempObject;
+    private List<GameObject> tempObjectList = new List<GameObject>();
+    private List<GameObject> usedObjectList = new List<GameObject>();
+    private int num;
+    private Image img;
+
+    public GameObject tempCenterObject;
+    public GameObject tempLeftUpObject;
+    public GameObject tempLeftDownObject;
+    public GameObject tempRightUpObject;
+    public GameObject tempRightDownObject;
+
+    public GameObject position0;
+    public GameObject position1;
+    public GameObject position2;
+    public GameObject position3;
+    public GameObject position4;
 
     private void Awake()
     {
@@ -13,21 +28,53 @@ public class ObjectGenerator : MonoBehaviour
     }
     void Start()
     {
-        GenerateObject();
+        GenerateCornerObjects();
+        GenerateCenterObject();
     }
 
-    public void GenerateObject()
+    public void GenerateCornerObjects()
     {
-        num = Random.Range(0, 3);
-      tempObject =  Instantiate(objectList[num], transform.position, transform.rotation, transform);
+        tempObjectList.AddRange(objectList);
+
+        CreateObject(ref tempLeftUpObject, position1.transform);
+        CreateObject(ref tempLeftDownObject, position2.transform);
+        CreateObject(ref tempRightUpObject, position3.transform);
+        CreateObject(ref tempRightDownObject, position4.transform);
     }
 
+    public void CreateObject(ref GameObject tempObject, Transform objTransform)
+    {
+        var objNum = Random.Range(0, tempObjectList.Count);
+
+        tempObject = Instantiate(tempObjectList[objNum], objTransform.position, objTransform.rotation, objTransform);
+        tempObjectList.RemoveAt(objNum);
+        usedObjectList.Add(tempObject);
+    }
+
+    public void GenerateCenterObject()
+    {
+        num = Random.Range(0, usedObjectList.Count);
+        tempCenterObject = Instantiate(usedObjectList[num], position0.transform.position, position0.transform.rotation, position0.transform);
+        img = tempCenterObject.GetComponent<Image>();
+        img.color = new Color(0, 0, 0);
+        img.raycastTarget = false;
+    }
+ 
     public void CheckObjects(string tag)
     {
-        if (tag.Equals(objectList[num].tag))
+        if (tag.Equals(usedObjectList[num].tag))
         {
-            Destroy(tempObject);
-            GenerateObject();
+            Destroy(tempCenterObject);
+            Destroy(tempLeftUpObject);
+            Destroy(tempLeftDownObject);
+            Destroy(tempRightDownObject);
+            Destroy(tempRightUpObject);
+
+            usedObjectList.Clear();
+            tempObjectList.Clear();
+
+            GenerateCornerObjects();
+            GenerateCenterObject();
         }
         else
         {
---
+++ Assets/Scripts/ObjectId.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public sealed class ObjectId : MonoBehaviour
+{
+    public int ID;
+}
